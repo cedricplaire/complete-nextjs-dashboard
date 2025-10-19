e7f66b0a75beed96a6f7cb1cbec94eb559c6ac43
Plaire Cédric <cedricplaire30@gmail.com>
2025-10-19 04:37:02 +0200

    chap13 ended

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,8 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
  
-export default async function Page() {
-    
+export default async function Page() {  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -24,7 +23,6 @@ export default async function Page() {
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
-
       </div>
     </main>
   );
